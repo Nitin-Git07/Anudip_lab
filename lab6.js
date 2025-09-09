00ebeee87677648bd3247fb309ab7d83bfc844fe
Nitin sharma <nitingit2003@gmail.com>
2025-09-09 13:53:52 +0530

    Add files via upload

---
+++ lab6.js
@@ -0,0 +1,9 @@
+let msg1 = "Hello, this is my first Node.js program!";
+let msg2 = "I am excited to learn more about Node.js!";
+
+let combined = msg1 + " " + msg2;
+
+console.log(combined);
+
+// output
+// Hello, this is my first Node.js program! I am excited to learn more about Node.js!
